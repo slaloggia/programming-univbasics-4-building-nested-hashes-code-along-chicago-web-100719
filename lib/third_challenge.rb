62130436dd9943f063819e12785ea53fa3754767
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-17 00:44:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,8 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot=headed"}]
+      :hero_friends => [ 
+        {name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"} ]
    }, 
    :capulet => {
       :patriarch => {
@@ -30,7 +31,7 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => [{name: "Steven", age: "44", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"}]
+      :heroine_friends => [ {name: "Steven", age: "44", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"} ]
    }
   }
 
